c69fc75cbe82f9f29fd791e0c6f1c30ed10d2bc0
walid <adil.walid.abdellah@gmail.com>
2022-01-12 19:05:57 +0100

    adding landing page

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+        $('.side_bar').mouseenter(function (e) { 
+            $('.side_bar').removeClass('active');
+        });
+        $('.side_bar').mouseleave(function (e) { 
+            $('.side_bar').toggleClass('active');
+        });
+        $('#disc').mouseenter(function (e) { 
+            $(this).toggleClass('show-more')
+        });
+        $('#disc').mouseleave(function (e) { 
+            $(this).removeClass('show-more')
+        });
+    });
